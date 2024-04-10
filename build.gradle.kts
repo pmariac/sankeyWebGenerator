9eebe43855463a8e060904ccbe7873beca1bc8ec
pierre <pierre@data2viz.io>
2024-04-10 15:42:58 +0200

    Use Data2viz 0.10.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,14 +6,13 @@ group = "io.data2viz"
 version = "1.0-SNAPSHOT"
 
 repositories {
-	mavenLocal()
     mavenCentral()
 }
 
 dependencies {
-	commonMainImplementation("io.data2viz.d2v:d2v-viz:0.10.7-SNAPSHOT")
-	commonMainImplementation("io.data2viz.d2v:d2v-sankey:0.10.7-SNAPSHOT")
-	commonMainImplementation("io.data2viz.d2v:d2v-format:0.10.7-SNAPSHOT")
+	commonMainImplementation("io.data2viz.d2v:d2v-viz:0.10.7")
+	commonMainImplementation("io.data2viz.d2v:d2v-sankey:0.10.7")
+	commonMainImplementation("io.data2viz.d2v:d2v-format:0.10.7")
 }
 
 kotlin {
